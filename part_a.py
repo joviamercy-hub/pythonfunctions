5e28a850a81dffb1ebfbe658b2c4e8844d9db106
practice <joviamercyikiror@gmail.com>
2025-11-05 15:35:50 +0300

    assignment about the python  functions

---
+++ part_a.py
@@ -0,0 +1,23 @@
+# 1.What is a function in Python?
+# A function is a block of reusable that performs a specific and related tasks
+
+# 2.Explain the difference between parameter and argument.
+# A parameter is a variable you pass to a function forexample def greet(name), name is the parameter
+# Argument is the actual value passed to the function when executed forexample greet("Jovia") Jovia is the argument
+
+# 3. What does the return keyword do?
+# the return keyword is used to send a value back from the function
+# forexample def subtract_numbers(8,2), result = 8-2 
+
+# 4.What is the output of this code?
+# def test(a, b=2): return a * b print(test(3))
+# a=3, returns 3 * 2 = 6
+
+
+
+
+
+
+
+
+
---
+++ part_b.py
@@ -0,0 +1,11 @@
+# 1.Write a function to calculate the square of a number.
+# def square(num): return num
+number = 5
+square = number ** 2
+print(square)
+
+# 2.Write a function that takes two numbers and returns their average.
+# def average(a, b): return (a + b) / 2
+
+
+
