f1a170e538bdae6a3ed3fb4cede1e4281a5b8948
practice <joviamercyikiror@gmail.com>
2025-11-05 16:26:23 +0300

    polished up work

--- part_b.py
+++ part_b.py
@@ -4,9 +4,42 @@ number = 5
 square = number ** 2
 print(square)
 
+
+
+
+
+
 # 2.Write a function that takes two numbers and returns their average.
-# def average(a, b): return (a + b) / 2
+def average(a,b): 
+    return (a+b)/2
+# print the results
+print (average (4,3))
+print(average (7,9))
 
 
 
+
+
+
+# 3. Write a function that checks if a number is even or odd.
+def even_or_odd(num) :
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
+
+#print results
+print(even_or_odd(5))
+print(even_or_odd(8))
+  
+     
+
+
+
+
+
+# references 
+# gemini AI
+# class notes 
 
